d72933d3ee9761c37c53929eadbba74069922116
Bright <mastereziono1@gmail.com>
2025-07-21 14:35:47 +0700

    Final success test - custom workflows enabled

--- policy-test-scenarios/expensive-instance/main.tf
+++ policy-test-scenarios/expensive-instance/main.tf
@@ -1,4 +1,4 @@
-# Test Case: Expensive instance type (should violate cost_control.rego) - FINAL TEST
+# Test Case: Expensive instance type (should violate cost_control.rego) - SUCCESS TEST
 resource "aws_instance" "test_expensive" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "m5.large"  # This should trigger cost control policy
