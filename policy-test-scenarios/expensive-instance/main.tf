4bdcb468a208418c9f3afb8744d798c8b53b8f83
Bright <mastereziono1@gmail.com>
2025-07-21 14:30:13 +0700

    Test repo config approach

--- policy-test-scenarios/expensive-instance/main.tf
+++ policy-test-scenarios/expensive-instance/main.tf
@@ -1,4 +1,4 @@
-# Test Case: Expensive instance type (should violate cost_control.rego) - TEST CONFIG
+# Test Case: Expensive instance type (should violate cost_control.rego) - REPO CONFIG TEST
 resource "aws_instance" "test_expensive" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "m5.large"  # This should trigger cost control policy
